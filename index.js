ee6bf39ac29b35921667b3b19e299c69db8585e0
kidonng <kidonng@gmail.com>
2019-05-30 00:18:26 +0800

    0.1.1

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ exports.apply = (api, { selector = 'img', option = {} } = {}) => {
   api.hooks.chainWebpack.tap(ID, config => {
     config.plugin('constants').tap(([options]) => [
       Object.assign(options, {
-        __MEDIUM_ZOOM_SELECTOR__: selector,
+        __MEDIUM_ZOOM_SELECTOR__: JSON.stringify(selector),
         __MEDIUM_ZOOM_OPTIONS__: option
       })
     ])
